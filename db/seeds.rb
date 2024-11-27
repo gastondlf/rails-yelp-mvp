e0e558b2c696f5f3385adad16ab7dde86b8e8e1b
Gaston de la Fare <gastondelafare@gmail.com>
2024-11-27 19:09:31 -0300

    completed challenge 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# 1. Clean database
+puts "Cleaning database..."
+Restaurant.destroy_all
+Review.destroy_all
+
+# 2. Create the instances
+category = ["chinese", "italian", "japanese", "french", "belgian"]
+puts "Creating Restaurants..."
+10.times do
+  restaurant = Restaurant.create!(address: Faker::Address.street_address, name: Faker::Restaurant.name, category: category.sample, phone_number: Faker::PhoneNumber.phone_number)
+  puts "Created #{restaurant.name}"
+end
+
+# 3. Display result
+puts "Finished! Successfully created #{Restaurant.count} restaurants"
